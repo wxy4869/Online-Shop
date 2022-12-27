c3bb299ff68321bc622e2775ab890dd9b4f7cd98
wxy4869 <wangxinyuan4869@163.com>
2022-12-27 10:02:01 +0800

    feat

--- cookbook3/config/routes.rb
+++ cookbook3/config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
+  resources :categories
+  resources :recipes
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get 'welcome/index' => 'welcome#index'
+  root 'welcome#index'
 end
