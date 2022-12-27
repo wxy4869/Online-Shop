df06ec044ca62152a00ecdd2756d1a7a835cfc35
wxy4869 <wangxinyuan4869@163.com>
2022-12-27 10:52:46 +0800

    feat

--- blog4/config/routes.rb
+++ blog4/config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+  resources :blogs
+  resources :blogs do
+    resources :comments
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root :to => 'blogs#index'
 end
