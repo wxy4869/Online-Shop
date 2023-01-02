63339fbe21e0408c1a0fcd7010825d6304ce260d
wxy4869 <wangxinyuan4869@163.com>
2023-01-02 20:14:20 +0800

    fix: product

--- project/app/controllers/welcome_controller.rb
+++ project/app/controllers/welcome_controller.rb
@@ -3,13 +3,17 @@ class WelcomeController < ApplicationController
   before_action :authenticate_user!, only: %i[ center ]
 
   def index
-    @products = Product.all.order("status ASC").order("created_at DESC")
+    @products = Product.all.order("status ASC").order("sales DESC")
   end
 
   def center
     @collect_cnt = Collect.where(user_id: current_user.id).count
     @cart_cnt = Cart.where(user_id: current_user.id).count
     @order_cnt = Order.where(user_id: current_user.id).count
-    @product_id = rand(1..10000) % Product.all.length + 1
+    @product_id = if Product.all.length == 0
+      0
+    else
+      rand(1..10000) % Product.all.length + 1
+    end
   end
 end
--- project/app/models/product.rb
+++ project/app/models/product.rb
@@ -1,4 +1,7 @@
 class Product < ApplicationRecord
+  validates :product_name, length: { minimum: 1,  maximum: 40 }
+  validates :detail, length: { maximum: 200 }
+
   has_many :product_features
   has_many :users, through: :collects
 end
